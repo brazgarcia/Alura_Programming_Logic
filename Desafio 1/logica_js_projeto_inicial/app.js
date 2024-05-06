3ff0c5f2a997a6177a808f444ee1c4f2c37101de
Braz da Silva Garcia <bdanielgarcia@hotmail.com>
2024-05-06 11:48:31 -0300

    Criando repositório

---
+++ Desafio 1/logica_js_projeto_inicial/app.js
@@ -0,0 +1,32 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroSecreto = 29;
+let chute = prompt("Escolha um número entre 1 e 30");
+
+if (chute == numeroSecreto) {
+    console.log("Isso aí! Você descobriu o número secreto (29)");
+}
+
+alert ("Boas vindas ao nosso site!");
+
+let nome = "Lua";
+let idade = 25;
+let numeroDevendas = 50;
+let saldoDisponivel = 1000;
+
+alert ("Erro! Preencha todos os campos");
+
+let mensagemDeErro = "Erro! Preencha todos os campos. (teste)";
+
+alert (mensagemDeErro);
+
+let nome_1 = prompt ("Qual é o seu nome?");
+
+let idade_1 = prompt ("Qual é sua idade?");
+
+if (idade_1 >= 18) {
+    alert ("Você tem idade suficiente para tirar habilitação!");
+}
+
+
+
+
